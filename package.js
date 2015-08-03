76195b72be1e7de4281219d01340a32a4f21f3f3
Clemens Wältken <clemens@waeltken.de>
2015-08-03 13:35:00 +0200

    Add package.js for meteor

---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'waeltken:leaflet',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'Simple package for leaflet v1.0.0-beta.1',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles([
+    'dist/leaflet.css',
+    'dist/leaflet-src.js',
+    'dist/images/layers-2x.png',
+    'dist/images/layers.png',
+    'dist/images/marker-icon-2x.png',
+    'dist/images/marker-icon.png',
+    'dist/images/marker-shadow.png'
+  ], 'client');
+});
